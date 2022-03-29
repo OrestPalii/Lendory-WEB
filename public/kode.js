6ae489a02f69bed06d5b42ebfacc8b88bbbb4f0b
Magzumko <93189028+Magzumko@users.noreply.github.com>
2022-03-29 17:39:46 +0300

    creating advertisement
    posibility to create advertisement

--- public/kode.js
+++ public/kode.js
@@ -1,1 +1,60 @@
+import { getDatabase } from "firebase/database";
 
+const database = getDatabase();
+
+import { getDatabase, ref, set } from "firebase/database";
+
+function writeUserData(userId, name, email, imageUrl) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId), {
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+
+import { getDatabase, ref, onValue} from "firebase/database";
+
+const db = getDatabase();
+const starCountRef = ref(db, 'posts/' + postId + '/starCount');
+onValue(starCountRef, (snapshot) => {
+  const data = snapshot.val();
+  updateStarCount(postElement, data);
+});
+
+import { getDatabase, ref, child, get } from "firebase/database";
+
+const dbRef = ref(getDatabase());
+get(child(dbRef, `users/${userId}`)).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
+
+function writeNewPost(uid, username, picture, title, body) {
+    const db = getDatabase();
+  
+    // A post entry.
+    const postData = {
+      author: username,
+      uid: uid,
+      body: body,
+      title: title,
+      starCount: 0,
+      authorPic: picture
+    };
+  
+    // Get a key for a new Post.
+    const newPostKey = push(child(ref(db), 'posts')).key;
+  
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    const updates = {};
+    updates['/posts/' + newPostKey] = postData;
+    updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+  
+    return update(ref(db), updates);
+  }
