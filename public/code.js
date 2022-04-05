e732dee665b3e63c26793a92ceac40256f40010d
Magzumko <93189028+Magzumko@users.noreply.github.com>
2022-04-05 11:08:59 +0300

    js classes

---
+++ public/code.js
@@ -0,0 +1,80 @@
+class Adv{
+    constructor(area, currency, description, floor, location, name, numberOfRooms, price, volunteering) {
+       this.area = area;
+       this.currency = currency;
+       this.description = description;
+       this.floor = floor;
+       this.location = location;
+       this.name = name;
+       this.numberOfRooms = numberOfRooms;
+       this.price = price;
+       this.volunteering = volunteering; 
+   }
+   set Name(newName){
+         this.name=newName
+     }
+     get Name(){
+         return this.name
+     }
+     set Description(newDescription){
+         this.description=newDescription
+     }
+     get Description(){
+         return this.description
+     }
+     set Location(newLocation){
+         this.location=newLocation
+     }
+     get Location(){
+         return this.location
+     }
+     set Currency(newCurrency){
+         this.currency=newCurrency
+     }
+     get Currency(){
+         return this.currency
+     }
+      set Price(newPrice){
+          this.price=newPrice
+       }
+     get Price(){ 
+         return this.price
+     }
+      set area(newArea){
+         this.area=newArea
+     }
+     get area(){
+         return this.area
+     }
+      set NumberOfRoom(newNumberOfRoom){
+         this.numberOfRoom=newNumberOfRoom
+     }
+     get NumberOfRoom(){
+         return this.numberOfRoom
+     }
+      set Flor(newFlor){
+         this.flor=newFlor
+     }
+     get Flor(){
+         return this.flor
+     }
+      set Volunteering(newVolunteering){
+         this.volunteering=newVolunteering
+     }
+     get Volunteering(){
+         return this.volunteering
+     }
+      set Images(newImages){
+         this.images=newImages
+     }
+     get Images(){
+         return this.images
+     }
+      set Creator(newCreator){
+         this.creator=newCreator
+     }
+     get Creator(){
+         return this.creator
+     }
+   }
+    
---
+++ public/test.js
@@ -0,0 +1,11 @@
+class Advtest{
+    constructor(area){
+        this.area = area;
+    }
+    set area(newArea){
+        this.area=newArea;
+    }
+    get area(){
+        return this.area;
+    }
+}
