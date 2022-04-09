5dbc6c0990e4015c80c4e0cf8405b136c002c18b
OrestPalii <paliy.o.2004@gmail.com>
2022-04-09 13:51:41 +0300

    write read data mac

--- public/shkolnik.js
+++ public/shkolnik.js
@@ -13,9 +13,18 @@ var firebaseConfig = {
 
 
 function save(scoring){
-    alert("пізда");
     firebase.database().ref("/").child("score/").update({
         score: scoring
     })
-    alert("пізда2");
+}  
+arr = []; 
+var adv;
+function getdata(){
+    firebase.database().ref("advertisement").on('value', function(snapshot){
+        snapshot.forEach(function(element){
+            adv = element.val();
+            arr.push(adv);
+        });
+    })
+    alert(arr[3].area);
 }
