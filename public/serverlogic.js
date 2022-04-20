5a857e78f46225936ef22079d8a821c488782c84
OrestPalii <paliy.o.2004@gmail.com>
2022-04-20 16:27:29 +0300

    advreview
    all/current adv download in js

--- public/serverlogic.js
+++ public/serverlogic.js
@@ -1,15 +1,15 @@
 //Initialising firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyCPf1_9AlKYGFEF0nHn3c1IGH8BecVSeig",
-    authDomain: "lendory-b5d8b.firebaseapp.com",
-    databaseURL: "https://lendory-b5d8b-default-rtdb.firebaseio.com",
-    projectId: "lendory-b5d8b",
-    storageBucket: "lendory-b5d8b.appspot.com",
-    messagingSenderId: "759222136421",
-    appId: "1:759222136421:web:8c38d800bca06e1e6ce08c"
-  };
+  apiKey: "AIzaSyCPf1_9AlKYGFEF0nHn3c1IGH8BecVSeig",
+  authDomain: "lendory-b5d8b.firebaseapp.com",
+  databaseURL: "https://lendory-b5d8b-default-rtdb.firebaseio.com",
+  projectId: "lendory-b5d8b",
+  storageBucket: "lendory-b5d8b.appspot.com",
+  messagingSenderId: "759222136421",
+  appId: "1:759222136421:web:8c38d800bca06e1e6ce08c"
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 //Arrays for images uploading
 var files1 = [];
 var files2 = [];
@@ -152,17 +152,29 @@ function setdata(name, description, location, floor, rooms, area, rent, currency
          option.value = '';
     } 
 
-// arr = []; 
-// var adv;
-// function getdata(){
-//     firebase.database().ref("advertisement").on('value', function(snapshot){
-//         snapshot.forEach(function(element){
-//             adv = element.val();
-//             arr.push(adv);
-//         });
-//     })
-//     alert(arr[3].area);
-// }
+  allAdv = []; 
+  var adv;
+  function getdata(){
+      firebase.database().ref("advertisementtest").on('value', function(snapshot){
+        snapshot.forEach(function(element){
+          adv = element.val();
+          allAdv.push(adv);
+          });
+      })
+  }
+
+var currentAdv;
+function getCurrentAdv(code){
+  firebase.database().ref("advertisementtest/" + code).on('value', function(snapshot){
+    currentAdv = snapshot.val();
+    if(currentAdv == null){
+      window.location = "404.html";
+      //alert("Такого оголошення не існує");
+    }
+    else
+      alert(currentAdv.name);
+  })
+}
 
 var currentUser = null;
 
