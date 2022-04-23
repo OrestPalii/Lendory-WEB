09ef6ef27905fdedba0de81940a25f8f9fd88e3c
RomanLesheha <roman.leshega2003@gmail.com>
2022-04-23 15:55:06 +0300

    verstka

---
+++ public/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:2,
+		adaptiveHeigth:true,
+		speed:1000,
+		infinite:false,
+		waitForAnimate:false
+	
+	
+	});
+});
+
