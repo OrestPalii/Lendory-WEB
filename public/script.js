650bd2b12d5546103458ea1a098925629acff604
OrestPalii <paliy.o.2004@gmail.com>
2022-04-25 21:38:28 +0300

    First working prototype
    Sign up/in system
    Main page
    Like system
    ADV Recomentations

---
+++ public/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:2,
+		adaptiveHeigth:true,
+		speed:1000,
+		infinite:false,
+		waitForAnimate:true,
+		autoplay:true,
+		autoplaySpeed:3000,
+	
+	
+	});
+});
+
---
+++ public/sign_in_out_logic.js
@@ -0,0 +1,49 @@
+//when user want to create new advertsement, function check is he logged in
+function adv_create_user_check(){
+    currentUser = null;
+    let keepLoggedIn = localStorage.getItem("keepLoggedIn");
+  
+    if(keepLoggedIn == "yes"){
+      currentUser = JSON.parse(localStorage.getItem('user'));
+    }
+  
+    else{
+      currentUser = JSON.parse(sessionStorage.getItem('user'));
+    }
+  
+    if(currentUser==null){
+      window.location = "SignIn.html";
+    }
+    else{
+      window.location = "advert.html";
+    }
+    }
+  
+    function advert_page_loading(){
+      currentUser = null;
+      let keepLoggedIn = localStorage.getItem("keepLoggedIn");
+    
+      if(keepLoggedIn == "yes"){
+        currentUser = JSON.parse(localStorage.getItem('user'));
+      }
+    
+      else{
+        currentUser = JSON.parse(sessionStorage.getItem('user'));
+      }
+    
+      if(currentUser==null){
+        window.location = "SignIn.html";
+      }
+    }
+
+      
+      
+        
+      
+      
+      function Signout(){
+        sessionStorage.removeItem('user');
+        localStorage.removeItem('user');
+        localStorage.removeItem('keepLoggedIn');
+        window.location = 'index.html'
+    }
