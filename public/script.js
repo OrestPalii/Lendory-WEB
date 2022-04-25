78797838ebe6be75ac60f253c55aecf2d42cac97
RomanLesheha <roman.leshega2003@gmail.com>
2022-04-25 16:24:09 +0300

    verstka
    slider

--- public/script.js
+++ public/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 	$('.slider').slick({
 		arrows:true,
-		dots:true,
+		dots:false,
 		slidesToShow:2,
 		adaptiveHeigth:true,
 		speed:1000,
 		infinite:false,
-		waitForAnimate:false
+		waitForAnimate:true,
+		autoplay:true,
+		autoplaySpeed:3000,
 	
 	
 	});
